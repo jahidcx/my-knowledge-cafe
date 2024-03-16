7424cb40a28b80f5926cb25b0e604c9c3cb5338a
jahidcx <muhdjahidhasan@gmail.com>
2024-03-16 20:39:03 +0600

    add more style

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,14 +1,37 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
+    console.log(blog);
+    const { title, cover, author,author_img, posted_date, reading_time,hashtags} = blog;
     return (
         <div>
-            
+            <img src={cover} alt={`cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p className=''>{posted_date}</p>
+
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <p className='text-3xl'>Title:{title}</p>
+            <p>
+                {
+                    hashtags.map((hash, idx)=>{
+                        <span key={idx}><a>#{hash}</a></span>
+                    })
+                }
+            </p>
         </div>
     );
 };
 
-blog.propTypes = {
+Blog.propTypes = {
     blog: PropTypes.object.isRequired
 }
 export default Blog;
